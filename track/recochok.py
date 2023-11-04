d3b51a33130646c6125486c90a1380b34e73a1cf
KOYAMA SHOYA <s1f102103189@iniad.org>
2023-11-04 23:36:46 +0900

    Update recochok.py

--- track/recochok.py
+++ track/recochok.py
@@ -1,11 +1,6 @@
 import sys
 
 def main(lines):
-    # このコードは標準入力と標準出力を用いたサンプルコードです。
-    # このコードは好きなように編集・削除してもらって構いません。
-    # ---
-    # This is a sample code to use stdin and stdout.
-    # Edit and remove this code as you like.
     red = False
     red_count = 0
     white = False
@@ -42,8 +37,27 @@ def main(lines):
             elif ((red == False) and (white == False)):
                 print("DD")
 
-if __name__ == '__main__':
-    lines = []
-    for l in sys.stdin:
-        lines.append(l.rstrip('\r\n'))
-    main(lines)
+import sys
+
+def main(lines):
+    mao = 0
+    count = 0
+    for i, v in enumerate(lines):
+        #print("line[{0}]: {1}".format(i, v))
+        if (i==0):
+            x = v
+        elif (i==1):
+            y = list(map(int. v.split(" ")))
+            mao = y[0]
+            for i in range(10**18):
+                mao -= y[1]
+                if mao <= 0:
+                    print("YES")
+                    print(count+1)
+                mao += y[2]
+                count += 1
+        print("NO")
+
+
+
+
